22c4f59dc87121177730aa160abbdc8faa944fd4
BullyWiiPlaza <bullywiihacks@gmail.com>
2023-06-15 22:35:41 +0200

    Initial commit

---
+++ UsingSol2WithCMake.cpp
@@ -0,0 +1,10 @@
+﻿#include <sol/sol.hpp>
+#include <cassert>
+
+int main() {
+    sol::state lua;
+    int x = 0;
+    lua.set_function("beep", [&x] { ++x; });
+    lua.script("beep()");
+    assert(x == 1);
+}
